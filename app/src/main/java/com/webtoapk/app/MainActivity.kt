1ee1e5342175afa625e5f68c7296daf8ce595b38
youns03 <negane470@gmail.com>
2026-03-15 12:54:41 +0000

    fix: Move MainActivity to unified package structure

---
+++ app/src/main/java/com/webtoapk/app/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.webtoapk.app
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.webkit.WebSettings
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        webView = WebView(this)
+        setContentView(webView)
+
+        val settings = webView.settings
+        settings.javaScriptEnabled = true
+        settings.domStorageEnabled = true
+        settings.allowFileAccess = true
+        settings.allowContentAccess = true
+        settings.loadWithOverviewMode = true
+        settings.useWideViewPort = true
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                return false // Load all links inside the WebView
+            }
+        }
+
+        // Load the local index.html from assets/www
+        webView.loadUrl("file:///android_asset/www/index.html")
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
